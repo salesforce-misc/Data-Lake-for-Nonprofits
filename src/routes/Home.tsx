c8a7c3389c4ddf5c5d3d23610a2a809501761c5c
Simsek Mert <simsek97@gmail.com>
2022-07-31 20:38:52 -0400

    Add brand logos

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -80,14 +80,7 @@ export const Home: FC = observer(() => {
       <CurvedBox bgGradient={theme.gradients.bgLight} />
 
       <Container maxW="container.lg" pt="20px" position="relative">
-        <Stack direction={{ base: "column", lg: "row" }} spacing={{ base: "0", lg: "0" }} align="center" justifyContent="center">
-          <Box maxW="300px">
-            <Image src={ezDataLakeTitleImage} />
-          </Box>
-          <Box maxW="400px">
-            <Image src={welcomeImage} />
-          </Box>
-        </Stack>
+        test
       </Container>
 
       <Container maxW="container.md" position="relative">

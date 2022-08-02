65a87b26eafac16a8b3796a2d8ff13796b89b889
Simsek Mert <simsek97@gmail.com>
2022-08-02 14:14:52 -0400

    Refactor UserAccessKey model

--- src/models/helpers/UserAccessKey.tsx
+++ src/models/helpers/UserAccessKey.tsx
@@ -15,36 +15,38 @@ export interface IRawAccessKey {
 /**
  * Represents a user access key (id + secret)
  */
-export const UserAccessKey = types
-  .model("UserAccessKey", {
-    id: "",
-    secret: "",
-    createDate: types.Date,
-    status: types.enumeration<TAccessKeyStatus>("AccessKeyStatus", Object.values(TAccessKeyStatus)),
-    stale: false,
-  })
 
-  .actions((self) => ({
-    setAccessKey(rawAccessKey: IRawAccessKey) {
-      self.createDate = rawAccessKey.createDate;
-      self.status = rawAccessKey.status;
-      if (rawAccessKey.secret) self.secret = rawAccessKey.secret;
-      self.stale = false;
-    },
+const userAccessKeyProps = {
+  id: "",
+  secret: "",
+  createDate: types.Date,
+  status: types.enumeration<TAccessKeyStatus>("AccessKeyStatus", Object.values(TAccessKeyStatus)),
+  stale: false,
+};
 
-    setStatus(status: TAccessKeyStatus) {
-      self.status = status;
-    },
+const userAccessKeyActions = (self) => ({
+  setAccessKey(rawAccessKey: IRawAccessKey) {
+    self.createDate = rawAccessKey.createDate;
+    self.status = rawAccessKey.status;
+    if (rawAccessKey.secret) self.secret = rawAccessKey.secret;
+    self.stale = false;
+  },
 
-    markStale() {
-      self.stale = true;
-    },
-  }))
+  setStatus(status: TAccessKeyStatus) {
+    self.status = status;
+  },
 
-  .views((self) => ({
-    get isActive(): boolean {
-      return self.status === TAccessKeyStatus.Active;
-    },
-  }));
+  markStale() {
+    self.stale = true;
+  },
+});
+
+const userAccessKeyViews = (self) => ({
+  get isActive(): boolean {
+    return self.status === TAccessKeyStatus.Active;
+  },
+});
+
+export const UserAccessKey = types.model("UserAccessKey", userAccessKeyProps).actions(userAccessKeyActions).views(userAccessKeyViews);
 
 export interface IUserAccessKey extends Instance<typeof UserAccessKey> {}

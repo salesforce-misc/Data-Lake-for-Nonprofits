3b26105a8f8c447410e12693e3b4abf3de4e52a5
Simsek Mert <simsek97@gmail.com>
2022-08-03 19:35:52 -0400

    Setup test utils to wrap ChakraProvider

---
+++ src/test-utils.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, RenderOptions } from "@testing-library/react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { theme } from "themes/orange";
+
+const CustomProvider = ({ children }: { children: React.ReactNode }) => {
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+};
+
+const customRender = (ui: React.ReactElement, options?: Omit<RenderOptions, "wrapper">) => render(ui, { wrapper: CustomProvider, ...options });
+
+export * from "@testing-library/react";
+
+export { customRender as render };

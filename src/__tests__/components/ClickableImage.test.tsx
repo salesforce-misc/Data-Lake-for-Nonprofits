5dea575d351d1f5acd31fb9f31c39fbcf8197d2f
Simsek Mert <simsek97@gmail.com>
2022-08-04 10:54:33 -0400

    Update unit tests

--- src/__tests__/components/ClickableImage.test.tsx
+++ src/__tests__/components/ClickableImage.test.tsx
@@ -4,30 +4,28 @@ import { render, RenderWithChakra } from "test-utils";
 
 import { ClickableImage } from "components/ClickableImage";
 
+const component = <ClickableImage src="https://d0.awsstatic.com/logos/powered-by-aws.png" title="Powered by AWS" />;
+
 describe("ClickableImage component", () => {
   afterEach(cleanup);
 
   test("snapshot", () => {
-    const tree = create(
-      <RenderWithChakra>
-        <ClickableImage src="https://d0.awsstatic.com/logos/powered-by-aws.png" title="Powered by AWS" />
-      </RenderWithChakra>
-    ).toJSON();
+    const tree = create(<RenderWithChakra>{component}</RenderWithChakra>).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   test("render properly", async () => {
-    render(<ClickableImage src="https://d0.awsstatic.com/logos/powered-by-aws.png" title="Powered by AWS" />);
+    render(component);
 
     expect(screen.getByAltText("Powered by AWS")).toBeTruthy();
     expect(screen.queryByRole("button")).not.toBeInTheDocument();
   });
 
   test("click event", async () => {
-    render(<ClickableImage src="https://d0.awsstatic.com/logos/powered-by-aws.png" title="Powered by AWS" />);
+    render(component);
 
-    // manually trigger the click event
+    // Trigger the click event
     fireEvent.click(screen.getByAltText("Powered by AWS"));
 
     await waitFor(() => screen.findByText("Close"));

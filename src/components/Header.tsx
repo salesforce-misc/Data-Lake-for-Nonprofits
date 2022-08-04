07e8a0bdbc7e9c46f74462bbbc52496b626bce62
Simsek Mert <simsek97@gmail.com>
2022-08-04 17:48:08 -0400

    Update logo and tests

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,9 @@ import { observer } from "mobx-react";
 import { Box, Stack, Heading, HStack, Image, VStack, StackDivider, Container } from "@chakra-ui/react";
 
 import { theme } from "themes/orange";
+
 import AWSLogo from "images/AWS_logo_RGB_BLK.png";
-import SalesforceLogo from "images/Salesforce-dot-org-Logo-CMYK-Hrzl.png";
+import SalesforceLogo from "images/Salesforce_Corporate_Logo_RGB.png";
 
 export const Header = observer(() => {
   return (
@@ -16,25 +17,16 @@ export const Header = observer(() => {
           divider={<StackDivider borderColor="gray.200" />}
           width="100%"
         >
-          <Box maxW="220px" maxH="100px" paddingRight="10px">
+          <Box maxW="90px" paddingRight="24px">
             <Image src={SalesforceLogo} />
           </Box>
-          <Box maxW="60px">
-            <Image src={AWSLogo} marginTop="8px" marginLeft="24px" />
+          <Box maxW="65px">
+            <Image src={AWSLogo} marginLeft="24px" />
           </Box>
         </HStack>
         <Stack direction={{ base: "column", lg: "row" }} spacing={{ base: "0", lg: "0" }} width="100%">
           <Box maxH="160px" marginBottom="32px">
-            <Heading
-              display="inline-block"
-              size="lg"
-              pt="10px"
-              pb="10px"
-              pl={2}
-              bgGradient={theme.gradients.bgDark}
-              bgClip="text"
-              letterSpacing="-1px"
-            >
+            <Heading display="inline-block" size="lg" pb="10px" bgGradient={theme.gradients.bgDark} bgClip="text" letterSpacing="-1px">
               Datalake for Nonprofits, Powered by AWS
             </Heading>
           </Box>

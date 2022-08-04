a8bbbc02be1bc14c5fbf9c686b3caf292e731304
Simsek Mert <simsek97@gmail.com>
2022-08-04 12:52:48 -0400

    Unit tests for CurvedBox component

--- src/components/CurvedBox.tsx
+++ src/components/CurvedBox.tsx
@@ -1,27 +1,29 @@
-import { FC } from "react";
+import React from "react";
 import { Box, BoxProps, useTheme } from "@chakra-ui/react";
 
 /**
  * A div with a curved bottom
+ * To generate a curved css section
+ * see https://cssmatic.net/css-curved-background-generator/
  */
-export const CurvedBox: FC<BoxProps> = (props) => {
-  // To generate a curved css section
-  // see https://cssmatic.net/css-curved-background-generator/
-
+export const CurvedBox = (props: BoxProps) => {
   const theme: any = useTheme();
 
-  const curvedStyle = {
-    display: "block",
-    boxSizing: "border-box",
-    bgGradient: theme.gradients?.bgDark,
-    clipPath: "ellipse(111% 100% at 65.86% 0%)",
-    position: "absolute",
-    left: 0,
-    top: 0,
-    height: { base: "500px", md: "450px" },
-    w: "full",
-    ...props,
-  };
-
-  return <Box sx={curvedStyle} />;
+  return (
+    <Box
+      sx={{
+        display: "block",
+        boxSizing: "border-box",
+        bgGradient: theme.gradients?.bgDark,
+        clipPath: "ellipse(111% 100% at 65.86% 0%)",
+        position: "absolute",
+        left: 0,
+        top: 0,
+        height: { base: "500px", md: "450px" },
+        w: "full",
+        ...props,
+      }}
+      data-testid="curved-box"
+    />
+  );
 };

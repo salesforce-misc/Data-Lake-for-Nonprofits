c562feb1c46b15a8e5f58ef058f97f09a64ea148
Simsek Mert <simsek97@gmail.com>
2022-08-05 15:01:58 -0400

    Unit tests for OutlineButton

--- app/src/__tests__/components/OutlineButton.test.tsx
+++ app/src/__tests__/components/OutlineButton.test.tsx
@@ -1,9 +1,8 @@
 import { cleanup, screen } from "@testing-library/react";
 import { create } from "react-test-renderer";
 
-import { render, RenderWithChakra } from "test-utils";
+import { RenderWithChakra } from "test-utils";
 
-import { Markdown } from "components/Markdown";
 import { OutlineButton } from "components/OutlineButton";
 
 describe("OutlineButton component", () => {

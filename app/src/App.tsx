20cfcdc88fdfa3aed54db79f162f08752d6a4c54
Simsek Mert <simsek97@gmail.com>
2022-08-07 08:26:16 -0400

    Update import paths

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,26 +3,27 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
 import { observer } from "mobx-react";
 
-import { theme as orange } from "./themes/orange";
-import { theme as purple } from "./themes/purple";
-import { theme as blue } from "./themes/blue";
-import { theme as pink } from "./themes/pink";
-import { theme as green } from "./themes/green";
-import { Home } from "./routes/home/Home";
-import { Step1 } from "./routes/step1/Step1";
-import { Step2 } from "./routes/step2/Step2";
-import { Step3 } from "./routes/step3/Step3";
-import { Step4 } from "./routes/step4/Step4";
-import { Step5 } from "./routes/step5/Step5";
-import { Step6 } from "./routes/step6/Step6";
+import { theme as orange } from "themes/orange";
+import { theme as purple } from "themes/purple";
+import { theme as blue } from "themes/blue";
+import { theme as pink } from "themes/pink";
+import { theme as green } from "themes/green";
 
-interface IThemeWrapper {
+import { Home } from "routes/home/Home";
+import { Step1 } from "routes/step1/Step1";
+import { Step2 } from "routes/step2/Step2";
+import { Step3 } from "routes/step3/Step3";
+import { Step4 } from "routes/step4/Step4";
+import { Step5 } from "routes/step5/Step5";
+import { Step6 } from "routes/step6/Step6";
+
+interface IChakraWrapper {
   theme: any;
   children?: React.ReactNode;
 }
 
 // A simple wrapper for ChakraProvider
-const ThemeWrapper = ({ theme, children }: IThemeWrapper) => {
+const ChakraWrapper = ({ theme, children }: IChakraWrapper) => {
   return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 };
 
@@ -37,14 +38,13 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<ThemeWrapper theme={orange} children={<Home />} />} />
-      {/* <Route path="/" element={<ThemeWrapper theme={purple} children={<Step6 />} />} /> */}
-      <Route path="/steps/1" element={<ThemeWrapper theme={orange} children={<Step1 />} />} />
-      <Route path="/steps/2" element={<ThemeWrapper theme={blue} children={<Step2 />} />} />
-      <Route path="/steps/3" element={<ThemeWrapper theme={purple} children={<Step3 />} />} />
-      <Route path="/steps/4" element={<ThemeWrapper theme={pink} children={<Step4 />} />} />
-      <Route path="/steps/5" element={<ThemeWrapper theme={green} children={<Step5 />} />} />
-      <Route path="/steps/6" element={<ThemeWrapper theme={purple} children={<Step6 />} />} />
+      <Route path="/" element={<ChakraWrapper theme={orange} children={<Home />} />} />
+      <Route path="/steps/1" element={<ChakraWrapper theme={orange} children={<Step1 />} />} />
+      <Route path="/steps/2" element={<ChakraWrapper theme={blue} children={<Step2 />} />} />
+      <Route path="/steps/3" element={<ChakraWrapper theme={purple} children={<Step3 />} />} />
+      <Route path="/steps/4" element={<ChakraWrapper theme={pink} children={<Step4 />} />} />
+      <Route path="/steps/5" element={<ChakraWrapper theme={green} children={<Step5 />} />} />
+      <Route path="/steps/6" element={<ChakraWrapper theme={purple} children={<Step6 />} />} />
       <Route
         path="*"
         element={

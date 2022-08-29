e1ed699e4122cd98f60d0d728b9174414bcb4415
Simsek Mert <simsek97@gmail.com>
2022-08-29 11:30:50 -0400

    Update step6 with layout

--- app/src/pages/step6/Step6.tsx
+++ app/src/pages/step6/Step6.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Button, Container, Heading, HStack, Text, useTheme } from "@chakra-ui/react";
+import { Box, Button, HStack } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { observer } from "mobx-react";
 
 import { useInstallation } from "AppContext";
-import { Header } from "components/Header";
-import { CurvedBox } from "components/CurvedBox";
-import { StepsBanner } from "components/StepsBanner";
+import { Layout } from "components/Layout";
 import { AccessInformation } from "pages/step6/AccessInformation";
 import { UsersPanel } from "pages/step6/users-panel/UsersPanel";
 import { useColorScheme } from "models/useColorScheme";
@@ -15,7 +13,6 @@ import { LaunchYourConnection } from "./LaunchYourConnection";
 import { AdminTools } from "./AdminTools";
 
 export const Step6 = observer(() => {
-  const theme = useTheme();
   const { tone, colorScheme } = useColorScheme();
   const installation = useInstallation();
   const navigate = useNavigate();
@@ -31,60 +28,32 @@ export const Step6 = observer(() => {
   };
 
   return (
-    <Box id="step6">
-      <CurvedBox bgGradient={theme.gradients.bgLight} />
-
-      <Header />
-
-      <Box position="relative">
-        <CurvedBox />
-        <StepsBanner current={6} />
-
-        <Container maxW="container.md" pt="16px" position="relative" pb={10}>
-          <Box float="right" fontSize="small" textAlign="center" color={tone(100)}>
-            <Text>Installation ID</Text>
-            <Text>{installation.id}</Text>
-          </Box>
-
-          <Heading display="inline-block" size="lg" pt="0px" pb="10px" color="white" letterSpacing="-1px">
-            Connect to Analytics
-          </Heading>
-
-          <Box color={tone(50)}>
-            Now that you have the data lake provisioned, you can connect to Tableau for analytics. Here you will gather all the necessary information
-            needed to make that connection.
-          </Box>
-
-          <Box borderRadius="lg" boxShadow="base" bg={tone(25)} mt={4} p={10} pb={4} position="relative">
-            <UsersPanel />
-          </Box>
-
-          <Box borderRadius="lg" boxShadow="base" bg={tone(25)} mt={4} p={10} pb={4} position="relative">
-            <AccessInformation />
-          </Box>
-
-          <Box borderRadius="lg" boxShadow="base" bg={tone(25)} mt={4} p={10} pb={4} position="relative">
-            <LaunchYourConnection />
-          </Box>
-
-          <Box borderRadius="lg" boxShadow="base" bg={tone(25)} mt={4} p={10} pb={4} position="relative">
-            <AdminTools />
-          </Box>
-
-          <HStack justifyContent="space-between" p={3} pt={6} mb={12}>
-            <Button
-              colorScheme={colorScheme}
-              size="md"
-              _hover={{ bg: tone(100) }}
-              leftIcon={<ArrowBackIcon />}
-              variant="ghost"
-              onClick={handlePrevious}
-            >
-              Previous
-            </Button>
-          </HStack>
-        </Container>
+    <Layout
+      step={6}
+      title="Connect to Analytics"
+      description="Now that you have the data lake provisioned, you can connect to Tableau for analytics. Here you will gather all the necessary information needed to make that connection."
+    >
+      <Box borderRadius="lg" boxShadow="base" bg={tone(25)} mt={4} p={10} pb={4} position="relative">
+        <UsersPanel />
       </Box>
-    </Box>
+
+      <Box borderRadius="lg" boxShadow="base" bg={tone(25)} mt={4} p={10} pb={4} position="relative">
+        <AccessInformation />
+      </Box>
+
+      <Box borderRadius="lg" boxShadow="base" bg={tone(25)} mt={4} p={10} pb={4} position="relative">
+        <LaunchYourConnection />
+      </Box>
+
+      <Box borderRadius="lg" boxShadow="base" bg={tone(25)} mt={4} p={10} pb={4} position="relative">
+        <AdminTools />
+      </Box>
+
+      <HStack justifyContent="space-between" p={3} pt={6} mb={12}>
+        <Button colorScheme={colorScheme} size="md" _hover={{ bg: tone(100) }} leftIcon={<ArrowBackIcon />} variant="ghost" onClick={handlePrevious}>
+          Previous
+        </Button>
+      </HStack>
+    </Layout>
   );
 });

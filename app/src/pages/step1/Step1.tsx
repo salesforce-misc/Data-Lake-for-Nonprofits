c6b8a1e94e3e98472df50e2c4f32fab52241bcc2
Simsek Mert <simsek97@gmail.com>
2022-08-29 09:59:27 -0400

    Use layout for step1

--- app/src/pages/step1/Step1.tsx
+++ app/src/pages/step1/Step1.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { Box, Container, Heading, HStack, Button, Collapse, Stack, useTheme } from "@chakra-ui/react";
+import { Box, Heading, HStack, Button, Collapse } from "@chakra-ui/react";
 import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons";
 import { observer } from "mobx-react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 import { useInstallation, useStore } from "AppContext";
-import { Header } from "components/Header";
-import { CurvedBox } from "components/CurvedBox";
-import { StepsIndicator } from "components/StepsIndicator";
+import { Layout } from "components/Layout";
 import { OutlineButton } from "components/OutlineButton";
 import { YesNoAnswer } from "models/steps/BaseStep";
 import { CredentialsValidationException } from "api/validate-credentials";
@@ -19,7 +17,6 @@ import { InstructionSection } from "pages/step1/InstructionSection";
 import { awsRegions } from "data/aws-regions";
 
 export const Step1 = observer(() => {
-  const theme = useTheme();
   const installation = useInstallation();
   const step = installation.connectToAwsStep;
   const needsAssistance = step.needsAssistance;
@@ -70,102 +67,72 @@ export const Step1 = observer(() => {
   } Region`;
 
   return (
-    <Box id="step1">
-      <CurvedBox bgGradient={theme.gradients.bgLight} />
-
-      <Header />
-
-      <Box position="relative">
-        <CurvedBox />
-        <Box bg="orange.400" w="full" position="relative">
-          <Container maxW="container.md" pt="15px" pb="15px">
-            <StepsIndicator current={1} />
-          </Container>
+    <Layout
+      step={1}
+      title="Connect to your AWS account"
+      description="We need your AWS admin credentials, specifically the access keys, so that we can connect to your AWS account and provision the
+        resources for the data lake."
+      explanation="Access keys consist of two parts: an access key id and a secret access key. These access keys must be for a user with admin permissions."
+      warning="If you haven't created access keys before, we will guide you through the process of obtaining the needed credentials."
+    >
+      <Box borderRadius="lg" boxShadow="base" bg="orange.50" mt={0} p={6}>
+        <Box p={0}>
+          <Heading size="md" pt="0px" pb="30px" color="orange.600" letterSpacing="-1px">
+            Would you need assistance in creating AWS admin access and secret keys?
+          </Heading>
+          <Box textAlign="right" w="full" mb={4}>
+            <OutlineButton id="step1-btn-need-assistance-yes" selected={needsAssistance === YesNoAnswer.Yes} onClick={handleAnswer(YesNoAnswer.Yes)}>
+              Yes
+            </OutlineButton>
+            <OutlineButton
+              id="step1-btn-need-assistance-no"
+              selected={needsAssistance === YesNoAnswer.No}
+              onClick={handleAnswer(YesNoAnswer.No)}
+              ml={3}
+            >
+              No
+            </OutlineButton>
+          </Box>
         </Box>
 
-        <Container maxW="container.md" pt="0px" position="relative">
-          <Heading display="inline-block" size="lg" pt="16px" pb="30px" color="gray.100" letterSpacing="-1px">
-            Connect to your AWS account
-          </Heading>
-
-          <Box color="gray.50" mb={10}>
-            <Stack direction="column" spacing="8px" align="left" justifyContent="space-between">
-              <Box>
-                We need your AWS <b>admin</b> credentials, specifically the <b>access keys</b>, so that we can connect to your AWS account and
-                provision the resources for the data lake.
-              </Box>
-              <Box>
-                Access keys consist of two parts: an access key id and a secret access key. These access keys must be for a user with admin
-                permissions.
-              </Box>
-              <Box>If you haven't created access keys before, we will guide you through the process of obtaining the needed credentials.</Box>
-            </Stack>
+        <Collapse in={needsAssistance === YesNoAnswer.Yes} animateOpacity>
+          <Box pt={6}>
+            <InstructionSection />
           </Box>
-
-          <Box borderRadius="lg" boxShadow="base" bg="orange.50" mt={0} p={6}>
-            <Box p={0}>
-              <Heading size="md" pt="0px" pb="30px" color="orange.600" letterSpacing="-1px">
-                Would you need assistance in creating AWS admin access and secret keys?
-              </Heading>
-              <Box textAlign="right" w="full" mb={4}>
-                <OutlineButton
-                  id="step1-btn-need-assistance-yes"
-                  selected={needsAssistance === YesNoAnswer.Yes}
-                  onClick={handleAnswer(YesNoAnswer.Yes)}
-                >
-                  Yes
-                </OutlineButton>
-                <OutlineButton
-                  id="step1-btn-need-assistance-no"
-                  selected={needsAssistance === YesNoAnswer.No}
-                  onClick={handleAnswer(YesNoAnswer.No)}
-                  ml={3}
-                >
-                  No
-                </OutlineButton>
-              </Box>
-            </Box>
-
-            <Collapse in={needsAssistance === YesNoAnswer.Yes} animateOpacity>
-              <Box pt={6}>
-                <InstructionSection />
-              </Box>
-            </Collapse>
-          </Box>
-
-          <Collapse in={needsAssistance !== YesNoAnswer.MissingAnswer} animateOpacity>
-            <CredentialsError exception={stepException} />
-
-            <Box pt={6}>
-              <CredentialsForm {...{ register, errors, isSubmitting, control }} />
-            </Box>
-
-            <Box pt={6}>
-              <RegionForm {...{ register, errors, isSubmitting }} description={regionDescription} defaultRegion={installation.region} />
-            </Box>
-          </Collapse>
-
-          <HStack justifyContent="space-between" p={3} pt={6} mb={12}>
-            <Button colorScheme="orange" size="md" leftIcon={<ArrowBackIcon />} disabled={isSubmitting} variant="ghost" onClick={handlePrevious}>
-              Previous
-            </Button>
-
-            <Button
-              id="step1-btn-next"
-              colorScheme="orange"
-              size="md"
-              loadingText="Connecting"
-              rightIcon={<ArrowForwardIcon />}
-              ml={3}
-              isLoading={isSubmitting}
-              disabled={needsAssistance === YesNoAnswer.MissingAnswer || isSubmitting}
-              onClick={handleSubmit(onSubmit)}
-            >
-              Next
-            </Button>
-          </HStack>
-        </Container>
+        </Collapse>
       </Box>
-    </Box>
+
+      <Collapse in={needsAssistance !== YesNoAnswer.MissingAnswer} animateOpacity>
+        <CredentialsError exception={stepException} />
+
+        <Box pt={6}>
+          <CredentialsForm {...{ register, errors, isSubmitting, control }} />
+        </Box>
+
+        <Box pt={6}>
+          <RegionForm {...{ register, errors, isSubmitting }} description={regionDescription} defaultRegion={installation.region} />
+        </Box>
+      </Collapse>
+
+      <HStack justifyContent="space-between" p={3} pt={6} mb={12}>
+        <Button colorScheme="orange" size="md" leftIcon={<ArrowBackIcon />} disabled={isSubmitting} variant="ghost" onClick={handlePrevious}>
+          Previous
+        </Button>
+
+        <Button
+          id="step1-btn-next"
+          colorScheme="orange"
+          size="md"
+          loadingText="Connecting"
+          rightIcon={<ArrowForwardIcon />}
+          ml={3}
+          isLoading={isSubmitting}
+          disabled={needsAssistance === YesNoAnswer.MissingAnswer || isSubmitting}
+          onClick={handleSubmit(onSubmit)}
+        >
+          Next
+        </Button>
+      </HStack>
+    </Layout>
   );
 });

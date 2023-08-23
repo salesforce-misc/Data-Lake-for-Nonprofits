498442c00e030552b25b43bf71264a0451caf57e
Aseidas Blauvelt <88823715+aseidasvera@users.noreply.github.com>
2023-08-23 12:57:55 +0100

    Update Header.tsx

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -6,6 +6,7 @@ import { Box, Stack, Heading, HStack, Image, VStack, StackDivider, Container, us
 
 import AWSLogo from "images/AWS_logo_RGB_BLK.png";
 import SalesforceLogo from "images/Salesforce_Corporate_Logo_RGB.png";
+import VeraLogo from "images/vera-solutions-squarelogo-1455866080574.png";
 
 export const Header = observer(() => {
   const theme = useTheme();
@@ -19,6 +20,9 @@ export const Header = observer(() => {
           divider={<StackDivider borderColor="gray.200" />}
           width="100%"
         >
+            <Box maxW="90px" paddingRight="24px">
+            <Image src={VeraLogo} />
+          </Box>
           <Box maxW="90px" paddingRight="24px">
             <Image src={SalesforceLogo} />
           </Box>

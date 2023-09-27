2fc38f1a7700728c4bb8d0cfbb0e0e04836e3207
Aseidas Blauvelt <88823715+aseidasvera@users.noreply.github.com>
2023-09-27 17:40:33 +0200

    Update Steps.ts

--- app/src/pages/home/Steps.ts
+++ app/src/pages/home/Steps.ts
@@ -10,13 +10,13 @@ export const steps: TStepInfo[] = [
   guide you through the process of obtaining the needed credentials`,
   },
   {
-    title: "Connect to your Salesforce organization",
-    desc: `For AWS to access your Salesforce data, we will need your authorization. We will guide you through the steps of setting up Amazon AppFlow which
+    title: "Connect to your Amp Impact Salesforce organization",
+    desc: `For AWS to access your Amp Impact Salesforce data, we will need your authorization. We will guide you through the steps of setting up Amazon AppFlow which
     is a service that allows AWS to connect to Salesforce, you will be creating a Salesforce connection, signing in to Salesforce and authorizing access`,
   },
   {
     title: "Select data import options",
-    desc: `In this step, you can pick the standard NPSP import options for your Salesforce data or customize these options to fit your needs`,
+    desc: `In this step, you can pick the standard Amp Impact import options for your Salesforce data or customize these options to fit your needs`,
   },
   {
     title: "Review and confirm",
